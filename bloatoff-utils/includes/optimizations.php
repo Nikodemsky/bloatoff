63cb1f9218762e24c4e50deacbb0cd46f45a135a
Nikodemsky <Nikodemh@gmail.com>
2025-11-26 20:18:37 +0100

    Refactored code, for better scalability.

--- bloatoff-utils/includes/optimizations.php
+++ bloatoff-utils/includes/optimizations.php
@@ -9,191 +9,171 @@ if (!defined('ABSPATH')) {
 
 /** List of optimizations **/
 
-// Gutenberg styles and scripts
+// Load all options
+$bloatoff_options = get_option('bloatoff_settings', array());
+
+// #01 Gutenberg styles and scripts
 function bu_remove_gutenberg_styles() {
-    $options = get_option('bloatoff_settings', array());
-    
-    if (!empty($options['gutenberg'])) {
-        wp_dequeue_style('wp-block-library');
-        wp_dequeue_style('wp-block-library-theme');
-        wp_dequeue_style('wc-blocks-style');
-        wp_dequeue_script('wp-editor');
-    }
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wc-blocks-style');
+    wp_dequeue_script('wp-editor');
 }
-add_action('wp_enqueue_scripts', 'bu_remove_gutenberg_styles', 100);
 
-// Emoji removal - styles and scripts
+// #02 Emoji removal - styles and scripts
 function bu_disable_emojis() {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['emojis'])) {
-        remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-        remove_action( 'wp_print_styles', 'print_emoji_styles' );
-        remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
-        remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-        remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
-        remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-        add_filter( 'tiny_mce_plugins', 'bu_disable_emojis_tinymce' );
-        add_filter( 'wp_resource_hints', 'bu_disable_emojis_remove_dns_prefetch', 10, 2 );
-    }
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 }
-add_action( 'init', 'bu_disable_emojis' );
 
-// Emoji removal - TinyMCE plugin
+// #02 Emoji removal - TinyMCE plugin
 function bu_disable_emojis_tinymce( $plugins ) {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['emojis'])) {
-        if ( is_array( $plugins ) ) {
-            return array_diff( $plugins, array( 'wpemoji' ) );
-        } else {
-            return array();
-        }
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
     }
+    return array();
 }
 
-// Emoji removal - CDN hostname from DNS prefetching hints.
+// #02 Emoji removal - CDN hostname from DNS prefetching hints.
 function bu_disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['emojis'])) {
-        if ( 'dns-prefetch' == $relation_type ) {
-            $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
-            $urls = array_diff( $urls, array( $emoji_svg_url ) );
-        }
-        return $urls;
+    if ( 'dns-prefetch' == $relation_type ) {
+        $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+        $urls = array_diff( $urls, array( $emoji_svg_url ) );
     }
+    return $urls;
 }
 
-// RSS feeds removal
+// #03 RSS feeds removal
 function bu_disable_feeds() {
-    $options = get_option('bloatoff_settings', array());
-    
-    if (!empty($options['rss'])) {
-        add_action('do_feed', 'bu_disable_feeds_callback', 1);
-        add_action('do_feed_rdf', 'bu_disable_feeds_callback', 1);
-        add_action('do_feed_rss', 'bu_disable_feeds_callback', 1);
-        add_action('do_feed_rss2', 'bu_disable_feeds_callback', 1);
-        add_action('do_feed_atom', 'bu_disable_feeds_callback', 1);
-        add_action('do_feed_rss2_comments', 'bu_disable_feeds_callback', 1);
-        add_action('do_feed_atom_comments', 'bu_disable_feeds_callback', 1);
+    add_action('do_feed', 'bu_disable_feeds_callback', 1);
+    add_action('do_feed_rdf', 'bu_disable_feeds_callback', 1);
+    add_action('do_feed_rss', 'bu_disable_feeds_callback', 1);
+    add_action('do_feed_rss2', 'bu_disable_feeds_callback', 1);
+    add_action('do_feed_atom', 'bu_disable_feeds_callback', 1);
+    add_action('do_feed_rss2_comments', 'bu_disable_feeds_callback', 1);
+    add_action('do_feed_atom_comments', 'bu_disable_feeds_callback', 1);
 
-        // Links removal from header
-        remove_action( 'wp_head', 'feed_links', 2 );
-        remove_action( 'wp_head', 'feed_links_extra', 3 );
-    }
+    // Links removal from header
+    remove_action( 'wp_head', 'feed_links', 2 );
+    remove_action( 'wp_head', 'feed_links_extra', 3 );
 }
-add_action('init', 'bu_disable_feeds');
 
 function bu_disable_feeds_callback() {
     wp_die(__('No feeds available!', 'bloatoff-utils'));
 }
 
-// Really Simple Discovery removal
+// #04 Really Simple Discovery removal
 function bu_disable_rsd() {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['rsdl'])) {
-        remove_action( 'wp_head', 'rsd_link' );
-    }
+    remove_action( 'wp_head', 'rsd_link' );
 }
-add_action('init', 'bu_disable_rsd');
 
-// Shortlink
+// #05 Shortlink removal
 function bu_disable_shortlink() {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['shortlink'])) {
-        remove_action('wp_head', 'wp_shortlink_wp_head', 10);
-    }
+    remove_action('wp_head', 'wp_shortlink_wp_head', 10);
 }
-add_action('init', 'bu_disable_rsd');
 
-// jQuery Migrate
-/*function bu_remove_jquery_migrate( $scripts ) {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['jquerymigrate'])) {
-        if ( ! is_admin() && isset( $scripts->registered['jquery'] ) ) {
-                $script = $scripts->registered['jquery'];
-        if ( $script->deps ) { 
-        // Check whether the script has any dependencies
-        $script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
-        }}
-    }
-}
-add_action( 'wp_default_scripts', 'bu_remove_jquery_migrate' );*/
-
-// Admin dashboard widgets
+// #06 Admin dashboard widgets
 function bu_remove_all_dashboard_widgets() {
-    $options = get_option('bloatoff_settings', array());
 
-    if (!empty($options['adminwidgets'])) {
+    // Remove Welcome Panel
+    remove_action( 'welcome_panel', 'wp_welcome_panel' );
 
-        // Remove Welcome Panel
-        remove_action( 'welcome_panel', 'wp_welcome_panel' );
-
-        // Remove Widgets
-        remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' ); // Quick Draft
-        remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' ); // Activity
-        remove_meta_box( 'dashboard_primary', 'dashboard', 'side' ); // WordPress News
-        remove_meta_box( 'dashboard_site_health', 'dashboard', 'normal' ); // Site Health
-        remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' ); // At a Glance
-    }
+    // Remove Widgets
+    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' ); // Quick Draft
+    remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' ); // Activity
+    remove_meta_box( 'dashboard_primary', 'dashboard', 'side' ); // WordPress News
+    remove_meta_box( 'dashboard_site_health', 'dashboard', 'normal' ); // Site Health
+    remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' ); // At a Glance
 }
-add_action( 'wp_dashboard_setup', 'bu_remove_all_dashboard_widgets' );
 
-// REST link in header
+// #07 REST link in header
 function bu_remove_rest_link() {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['restapilink'])) {
-        remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
-    }
+    remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
 }
-add_action( 'after_setup_theme', 'bu_remove_rest_link' );
 
-// oEmbed discovery link in header
+// #08 oEmbed discovery link in header
 function bu_remove_oembed_link() {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['oembeddisclink'])) {
-        remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
-    }
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
 }
-add_action( 'after_setup_theme', 'bu_remove_oembed_link' );
 
-// Native XML Sitemap
+// #09 Native XML Sitemap
 function bu_disable_native_sitemap() {
-    $options = get_option('bloatoff_settings', array());
-    
-    if (!empty($options['nativexmlsitemap'])) {
-        add_filter('wp_sitemaps_enabled', '__return_false');
-    }
+    add_filter('wp_sitemaps_enabled', '__return_false');
 }
-add_action('init', 'bu_disable_native_sitemap');
 
-// Admin help tabs
+// #10 Admin help tabs
 function bu_remove_help_tabs($old_help, $screen_id, $screen){
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['adminhelptabs'])) {
-        $screen->remove_help_tabs();
-        return $old_help;
-    }
+    $screen->remove_help_tabs();
+    return $old_help;
 }
-add_filter( 'contextual_help', 'bu_remove_help_tabs', 999, 3 );
 
-// WP logo submenu and thank you message 
-// WP Logo function taken from: https://wordpress.org/plugins/remove-admin-bar-logo
+// #11 WP logo submenu and thank you message, the function has been taken from: https://wordpress.org/plugins/remove-admin-bar-logo
 function bu_admin_bar_remove_logo() {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['wplogoty'])) {
-        global $wp_admin_bar;
-        $wp_admin_bar->remove_menu( 'wp-logo' );
-        add_filter( 'admin_footer_text', fn () => '', 99, 0 );
-    }
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu( 'wp-logo' );
+    add_filter( 'admin_footer_text', fn () => '', 99, 0 );
 }
-add_action('wp_before_admin_bar_render','bu_admin_bar_remove_logo', 0 );
+
+/** List of checks **/
+
+// #01 Gutenberg styles and scripts
+if (!empty($bloatoff_options['gutenberg'])) {
+    add_action('wp_enqueue_scripts', 'bu_remove_gutenberg_styles', 100);
+}
+
+// #02 Emoji
+if (!empty($bloatoff_options['emojis'])) {
+    add_action( 'init', 'bu_disable_emojis' );
+    add_filter( 'tiny_mce_plugins', 'bu_disable_emojis_tinymce' );
+    add_filter( 'wp_resource_hints', 'bu_disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+
+// #03 RSS feeds
+if (!empty($bloatoff_options['rss'])) {
+    add_action('init', 'bu_disable_feeds');
+}
+
+// #04 Really Simple Discovery
+if (!empty($bloatoff_options['rsdl'])) {
+    add_action('init', 'bu_disable_rsd');
+}
+
+// #05 Shortlink
+if (!empty($bloatoff_options['shortlink'])) {
+    add_action('init', 'bu_disable_shortlink');
+}
+
+// #06 Admin dashboard widgets
+if (!empty($bloatoff_options['adminwidgets'])) {
+    add_action( 'wp_dashboard_setup', 'bu_remove_all_dashboard_widgets' );
+}
+
+// #07 REST link in header
+if (!empty($bloatoff_options['restapilink'])) {
+    add_action( 'after_setup_theme', 'bu_remove_rest_link' );
+}
+
+// #08 oEmbed discovery link in header
+if (!empty($bloatoff_options['oembeddisclink'])) {
+    add_action( 'after_setup_theme', 'bu_remove_oembed_link' );
+}
+
+// #09 Native XML Sitemap
+if (!empty($bloatoff_options['nativexmlsitemap'])) {
+    add_action('init', 'bu_disable_native_sitemap');
+}
+
+// #10 Admin help tabs
+if (!empty($bloatoff_options['adminhelptabs'])) {
+    add_filter( 'contextual_help', 'bu_remove_help_tabs', 999, 3 );
+}
+
+// #11 WP logo submenu and thank you message
+if (!empty($bloatoff_options['wplogoty'])) {
+    add_action('wp_before_admin_bar_render','bu_admin_bar_remove_logo', 0 );
+}
--- bloatoff-utils/includes/utilities.php
+++ bloatoff-utils/includes/utilities.php
@@ -9,127 +9,140 @@ if (!defined('ABSPATH')) {
 
 /** List of available utilities **/
 
-// Comments system removal
+// Load all options
+$bloatoff_options = get_option('bloatoff_settings', array());
+
+// #01 Comments system removal
 function bu_disable_comments() {
-    $options = get_option('bloatoff_settings', array());
-    
-    if (!empty($options['comments'])) {
+
+    // Redirect comments and discussion pages
+    add_action('admin_init', function () {
+        global $pagenow;
         
-        // Redirect comments and discussion pages
-        add_action('admin_init', function () {
-            global $pagenow;
-            
-            if ($pagenow === 'edit-comments.php' || $pagenow === 'options-discussion.php') {
-                wp_redirect(admin_url());
-                exit;
-            }
-
-            // Remove comments dashboard widget
-            remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
-
-            // Remove comments support from all post types
-            foreach (get_post_types() as $post_type) {
-                if (post_type_supports($post_type, 'comments')) {
-                    remove_post_type_support($post_type, 'comments');
-                    remove_post_type_support($post_type, 'trackbacks');
-                }
-            }
-        });
-
-        // Close comments and pings
-        add_filter('comments_open', '__return_false', 20, 2);
-        add_filter('pings_open', '__return_false', 20, 2);
-        add_filter('comments_array', '__return_empty_array', 10, 2);
-
-        // Remove comments from admin menu
-        add_action('admin_menu', function () {
-            remove_menu_page('edit-comments.php');
-            remove_submenu_page('options-general.php', 'options-discussion.php');
-        });
-
-        // Remove comments from admin bar
-        add_action('init', function () {
-            if (is_admin_bar_showing()) {
-                remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
-            }
-        });
-
-        // Remove comments from admin bar (alternative method)
-        add_action('wp_before_admin_bar_render', function () {
-            global $wp_admin_bar;
-            $wp_admin_bar->remove_menu('comments');
-        });
-    }
-}
-add_action('init', 'bu_disable_comments');
-
-// Widgets removal
-function bu_remove_widget_support() {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['widgets'])) {
-        remove_theme_support( 'widgets-block-editor' );
-        remove_theme_support( 'widgets' );
-    }
-}
-add_action( 'after_setup_theme', 'bu_remove_widget_support' );
-
-// oEmbed restriction
-function bu_disable_oembed_on_site() {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['oembed'])) {
-        if (!is_admin()) {
-            remove_filter( 'the_content', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
-            remove_action( 'rest_api_init', 'wp_oembed_register_route' );
-            add_filter( 'embed_oembed_discover', '__return_false' );
-            add_filter( 'embed_preview', '__return_false' );
+        if ($pagenow === 'edit-comments.php' || $pagenow === 'options-discussion.php') {
+            wp_redirect(admin_url());
+            exit;
         }
-    }
-}
-add_action( 'init', 'bu_disable_oembed_on_site' );
 
-// XML-RPC removal
-function bu_disable_xmlrpc() {
-    $options = get_option('bloatoff_settings', array());
-    
-    if (!empty($options['xmlrpc'])) {
-        add_filter('xmlrpc_enabled', '__return_false');
-    }
-}
-add_action('init', 'bu_disable_xmlrpc');
+        // Remove comments dashboard widget
+        remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
 
-// Self pingbacks removal
-function bu_no_self_ping( &$links ) {
-    $options = get_option('bloatoff_settings', array());
-
-    if (!empty($options['selfping'])) {
-        $home = get_option( 'home' );
-        foreach ( $links as $l => $link )
-            if ( 0 === strpos( $link, $home ) )
-                unset($links[$l]);
-    }
-}
-add_action( 'pre_ping', 'bu_no_self_ping' );
-
-// Heartbeat interval
-function bu_modify_heartbeat() {
-    $options = get_option('bloatoff_settings', array());
-    
-    if (!empty($options['heartbeat_enabled'])) {
-        add_filter('heartbeat_settings', function($settings) use ($options) {
-            $interval = isset($options['heartbeat_interval']) ? absint($options['heartbeat_interval']) : 15;
-            
-            // Ensure interval is within valid range
-            if ($interval < 1) {
-                $interval = 1;
-            } elseif ($interval > 86400) {
-                $interval = 86400;
+        // Remove comments support from all post types
+        foreach (get_post_types() as $post_type) {
+            if (post_type_supports($post_type, 'comments')) {
+                remove_post_type_support($post_type, 'comments');
+                remove_post_type_support($post_type, 'trackbacks');
             }
-            
-            $settings['interval'] = $interval;
-            return $settings;
-        });
+        }
+    });
+
+    // Close comments and pings
+    add_filter('comments_open', '__return_false', 20, 2);
+    add_filter('pings_open', '__return_false', 20, 2);
+    add_filter('comments_array', '__return_empty_array', 10, 2);
+
+    // Remove comments from admin menu
+    add_action('admin_menu', function () {
+        remove_menu_page('edit-comments.php');
+        remove_submenu_page('options-general.php', 'options-discussion.php');
+    });
+
+    // Remove comments from admin bar
+    add_action('init', function () {
+        if (is_admin_bar_showing()) {
+            remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+        }
+    });
+
+    // Remove comments from admin bar (alternative method)
+    add_action('wp_before_admin_bar_render', function () {
+        global $wp_admin_bar;
+        $wp_admin_bar->remove_menu('comments');
+    });
+}
+
+// #02 Widgets removal
+function bu_remove_widget_support() {
+    remove_theme_support( 'widgets-block-editor' );
+    remove_theme_support( 'widgets' );
+}
+
+// #03 oEmbed restriction
+function bu_disable_oembed_on_site() {
+    if (!is_admin()) {
+        remove_filter( 'the_content', array( $GLOBALS['wp_embed'], 'autoembed' ), 8 );
+        remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+        add_filter( 'embed_oembed_discover', '__return_false' );
+        add_filter( 'embed_preview', '__return_false' );
     }
 }
-add_action('init', 'bu_modify_heartbeat');
+
+// #04 XML-RPC removal - https://wordpress.stackexchange.com/a/219666 + https://wordpress.stackexchange.com/a/416999
+function bu_disable_xmlrpc() {    
+    add_filter('xmlrpc_enabled', '__return_false');
+}
+
+// #04 XML-RPC removal - pingbacks
+function disable_x_pingback( $headers ) {
+    unset( $headers['X-Pingback'] );
+    return $headers;
+}
+
+// #05 Self pingbacks removal
+function bu_no_self_ping( &$links ) {
+    $home = get_option( 'home' );
+    foreach ( $links as $l => $link )
+        if ( 0 === strpos( $link, $home ) )
+            unset($links[$l]);
+}
+
+// #06 Heartbeat interval
+function bu_modify_heartbeat() {
+    add_filter('heartbeat_settings', function($settings) use ($options) {
+        $interval = isset($bloatoff_options['heartbeat_interval']) ? absint($bloatoff_options['heartbeat_interval']) : 15;
+        
+        // Ensure interval is within valid range
+        if ($interval < 1) {
+            $interval = 1;
+        } elseif ($interval > 86400) {
+            $interval = 86400;
+        }
+        
+        $settings['interval'] = $interval;
+        return $settings;
+    });
+}
+
+/** List of checks **/
+
+// #01 Comments system
+if (!empty($bloatoff_options['comments'])) {
+    add_action('init', 'bu_disable_comments');
+}
+
+// #02 Widgets
+if (!empty($bloatoff_options['widgets'])) {
+    add_action( 'after_setup_theme', 'bu_remove_widget_support' );
+}
+
+// #03 oEmbed
+if (!empty($bloatoff_options['oembed'])) {
+    add_action( 'init', 'bu_disable_oembed_on_site' );
+}
+
+// #04 XML-RPC
+if (!empty($bloatoff_options['xmlrpc'])) {
+    add_action('init', 'bu_disable_xmlrpc');
+    add_filter('wp_headers', 'disable_x_pingback' );
+    add_filter('xmlrpc_methods', '__return_empty_array' );
+}
+
+// #05 Self pingbacks
+if (!empty($bloatoff_options['selfping'])) {
+    add_action( 'pre_ping', 'bu_no_self_ping' );
+}
+
+// #06 Heartbeat
+if (!empty($bloatoff_options['heartbeat_enabled'])) {
+    add_action('init', 'bu_modify_heartbeat');
+}
